0b1cdb491470b64442a1ca792f46019f8626f411
Parsa Hosseini <officer47p@gmail.com>
2024-03-09 00:13:29 +0330

    Add animations using gsap

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import "./style.css"
+import gsap from "gsap";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 // Sizes
@@ -67,3 +68,11 @@ const loop = () => {
   window.requestAnimationFrame(loop)
 }
 loop()
+
+// Timeline magiccc
+const tl = gsap.timeline({defaults: {
+  duration: 1
+}})
+tl.fromTo(mesh.scale, {z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1})
+tl.fromTo("nav", {y: "-100%"}, {y: "0%"})
+tl.fromTo(".title", {opacity: 0}, {opacity: 1})
