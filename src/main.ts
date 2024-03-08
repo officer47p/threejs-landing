c730002f75024bf8559092e6f1a54854ce95f5d1
Parsa Hosseini <officer47p@gmail.com>
2024-03-09 00:24:06 +0330

    Add color animation on mouse move

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,13 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5
 });
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Light
-const light = new THREE.PointLight(0xffffff, 70, 100, 1.7)
+const light = new THREE.PointLight(0xffffff, 100, 100, 1.7)
 light.position.set(0, 10, 10)
 scene.add(light)
 
@@ -76,3 +77,22 @@ const tl = gsap.timeline({defaults: {
 tl.fromTo(mesh.scale, {z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1})
 tl.fromTo("nav", {y: "-100%"}, {y: "0%"})
 tl.fromTo(".title", {opacity: 0}, {opacity: 1})
+
+
+// Mouse Animation Colorrrr
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => mouseDown = true)
+window.addEventListener("mouseup", () => mouseDown = false)
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX/sizes.width) * 255),
+      Math.round((e.pageY/sizes.height) * 255),
+      150,
+    ]
+    // Lets animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    gsap.to(mesh.material.color, {r: newColor.r, g: newColor.g, b: newColor.b})
+  }
+})
